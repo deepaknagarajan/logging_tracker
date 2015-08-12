b8edcd16557fac1815c170ba45a1c1f36a0f1e8b
deepak <deepak@sais-iMac.local>
2015-08-12 01:42:52 -0400

    sai first commit

---
+++ logger.pm
@@ -0,0 +1,30 @@
+#sreesaibaba
+package logger;
+use strict;
+use warnings;
+use Term::ANSIColor;
+use Storable;
+use Data::Dumper;
+
+
+sub s_logger
+{
+  my ($file) = @_;
+  unless (defined $file)
+  {
+    print "pass file to option\n";
+    exit;
+  }
+  print "file $file\n";
+}
+
+unless (caller())
+{
+  my $selector = shift @ARGV;
+  if ($selector == 1)
+  {
+    s_logger(@ARGV);
+  }
+}
+
+1
